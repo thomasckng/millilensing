93f68df83261a991782e4256edc4c7514500f3e0
Thomas Ng <thomas.ng@link.cuhk.edu.hk>
2023-04-19 12:02:08 +0800

    debug ref param

--- flowMC_test/GW150914/GW150914.py
+++ flowMC_test/GW150914/GW150914.py
@@ -90,27 +90,36 @@ L1_response = make_detector_response(L1[0], L1[1])
 #     hp, hc = gen_IMRPhenomD_polar(f, theta_waveform, f_ref)
 #     return L1_response(f, hp, hc, ra, dec, gmst, theta[8]) * jnp.exp(-1j*2*jnp.pi*f*(epoch+theta[5]))
 
-# def LogLikelihood(theta):
-#     theta_waveform = theta[:8]
-#     theta_waveform = theta_waveform.at[5].set(0)
-#     ra = theta[9]
-#     dec = theta[10]
-#     hp_test, hc_test = gen_IMRPhenomD_polar(H1_frequency, theta_waveform, f_ref)
-#     align_time = jnp.exp(-1j*2*jnp.pi*H1_frequency*(epoch+theta[5]))
-#     h_test_H1 = H1_response(H1_frequency, hp_test, hc_test, ra, dec, gmst, theta[8]) * align_time
-#     h_test_L1 = L1_response(L1_frequency, hp_test, hc_test, ra, dec, gmst, theta[8]) * align_time
-#     df = H1_frequency[1] - H1_frequency[0]
-#     match_filter_SNR_H1 = 4*jnp.sum((jnp.conj(h_test_H1)*H1_data)/H1_psd*df).real
-#     match_filter_SNR_L1 = 4*jnp.sum((jnp.conj(h_test_L1)*L1_data)/L1_psd*df).real
-#     optimal_SNR_H1 = 4*jnp.sum((jnp.conj(h_test_H1)*h_test_H1)/H1_psd*df).real
-#     optimal_SNR_L1 = 4*jnp.sum((jnp.conj(h_test_L1)*h_test_L1)/L1_psd*df).real
+@jax.jit
+def negative_LogLikelihood(theta):
+    theta_waveform = theta[:8]
+    theta_waveform = theta_waveform.at[5].set(0)
+    ra = theta[9]
+    dec = theta[10]
+    hp_test, hc_test = gen_IMRPhenomD_polar(H1_frequency, theta_waveform, f_ref)
+    align_time = jnp.exp(-1j*2*jnp.pi*H1_frequency*(epoch+theta[5]))
+    h_test_H1 = H1_response(H1_frequency, hp_test, hc_test, ra, dec, gmst, theta[8]) * align_time
+    h_test_L1 = L1_response(L1_frequency, hp_test, hc_test, ra, dec, gmst, theta[8]) * align_time
+    df = H1_frequency[1] - H1_frequency[0]
+    match_filter_SNR_H1 = 4*jnp.sum((jnp.conj(h_test_H1)*H1_data)/H1_psd*df).real
+    match_filter_SNR_L1 = 4*jnp.sum((jnp.conj(h_test_L1)*L1_data)/L1_psd*df).real
+    optimal_SNR_H1 = 4*jnp.sum((jnp.conj(h_test_H1)*h_test_H1)/H1_psd*df).real
+    optimal_SNR_L1 = 4*jnp.sum((jnp.conj(h_test_L1)*h_test_L1)/L1_psd*df).real
 
-#     return (match_filter_SNR_H1-optimal_SNR_H1/2) + (match_filter_SNR_L1-optimal_SNR_L1/2)
+    return -((match_filter_SNR_H1-optimal_SNR_H1/2) + (match_filter_SNR_L1-optimal_SNR_L1/2))
 
+optimize_prior_range = jnp.array([[20,40],[0.2,0.25],[-1,1],[-1,1],[10,2000],[-0.1,0.1],[0,2*np.pi],[0,np.pi],[0,np.pi],[0,2*np.pi],[-np.pi/2,np.pi/2]])
 
-ref_param = jnp.array([ 3.10497857e+01,  2.46759666e-01,  3.04854781e-01, -4.92774588e-01,
+import scipy
+
+print("Calculating the reference parameters")
+optimize_result = scipy.optimize.differential_evolution(negative_LogLikelihood, optimize_prior_range, maxiter=10000, workers=-1)
+ref_param = jnp.array(optimize_result.x)
+print("Reference parameters: ", ref_param)
+
+print(jnp.array([ 3.10497857e+01,  2.46759666e-01,  3.04854781e-01, -4.92774588e-01,
         5.47223231e+02,  1.29378808e-02,  3.30994042e+00,  3.88802965e-01,
-        3.41074151e-02,  2.55345319e+00, -9.52109059e-01])
+        3.41074151e-02,  2.55345319e+00, -9.52109059e-01]))
 
 from jaxgw.PE.heterodyneLikelihood import make_heterodyne_likelihood_mutliple_detector
 
@@ -149,7 +158,6 @@ rng_key_set = initialize_rng_keys(n_chains, seed=42)
 print("Initializing MCMC model and normalizing flow model.")
 
 prior_range = jnp.array([[10,80],[0.125,1.0],[-1,1],[-1,1],[0,2000],[-0.1,0.1],[0,2*np.pi],[-1,1],[0,np.pi],[0,2*np.pi],[-1,1]])
-optimize_prior_range = jnp.array([[10,80],[0.2,0.25],[-1,1],[-1,1],[0,2000],[-0.1,0.1],[0,2*np.pi],[0,np.pi],[0,np.pi],[0,2*np.pi],[-np.pi/2,np.pi/2]])
 
 
 initial_position = jax.random.uniform(rng_key_set[0], shape=(int(n_chains), n_dim)) * 1
